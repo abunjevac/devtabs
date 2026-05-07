597d685a95275e5df9cd5dd7c7b8d843dbc5ed0d
Alan Bunjevac <alan.bunjevac@gmail.com>
2026-05-07 15:07:26 +0200

    Resolve linter warning on global version variable

--- internal/version/version.go
+++ internal/version/version.go
@@ -2,4 +2,6 @@ package version
 
 // Version is set at build time via -ldflags "-X github.com/abunjevac/devtabs/internal/version.Version=vX.Y.Z".
 // Falls back to "dev" for local builds without a tag.
+//
+//nolint:gochecknoglobals
 var Version = "dev"
