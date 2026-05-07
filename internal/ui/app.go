2a4e7f9a03191d2bb0b9f6898c5b02e3e48eed57
Alan Bunjevac <alan.bunjevac@gmail.com>
2026-05-07 10:10:08 +0200

    Application window activator

---
+++ internal/ui/app.go
@@ -0,0 +1,22 @@
+package ui
+
+import (
+	"os"
+
+	"github.com/abunjevac/devtabs/internal/config"
+	"github.com/diamondburned/gotk4/pkg/gio/v2"
+	"github.com/diamondburned/gotk4/pkg/gtk/v4"
+)
+
+// Run starts the GTK application. Blocks until the window is closed.
+func Run(cfg *config.Config) {
+	app := gtk.NewApplication("io.github.abunjevac.devtabs", gio.ApplicationFlagsNone)
+
+	app.ConnectActivate(func() {
+		w := newWindow(app, cfg)
+
+		w.Present()
+	})
+
+	os.Exit(app.Run(os.Args[:1]))
+}
